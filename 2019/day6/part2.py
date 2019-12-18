0d767f51adbcdf1f6f276e3346515ae45fce87d3
Camilo Rivera <damelines@gmail.com>
2019-12-18 19:27:23 +1100

    Part 2 - algo explores parent as well

--- 2019/day6/part2.py
+++ 2019/day6/part2.py
@@ -5,12 +5,12 @@ import fileinput
 class SpaceObject:
     def __init__(self, name):
         self.name = name
-        self.around = None
+        self.parent = None
         self.children = []
     
-    def orbits(self, around):
-        around.children.append(self)
-        self.around = around
+    def orbits(self, parent):
+        parent.children.append(self)
+        self.parent = parent
 
     def display(self, indent = 0):
         indent_str = ''.ljust(indent, ' ')
@@ -27,34 +27,40 @@ class SpaceObject:
             if (o is not None):
                 return o
         return None
+    
+    def is_com(self):
+        return self.parent is None
 
     def find_orbital_transfers(self, name, orb_transfers = 0, visited = set()):
-        if (self.name in visited):
-            return None # object has already been visited
-        visited.add(self.name) # visit it
+        print("Exploring %s with %d transfers" % (self.name, orb_transfers))
+
+        if (self.is_com()):
+            return None
 
         if (self.name == name):
-            print('Found name ' + name)
+            print("Found %s with %d transfers" % (name, orb_transfers))
             return orb_transfers
 
-        transfers = []
+        if (self.name in visited):
+            print("%s has already been visited" % self.name)
+            return None
+
+        visited.add(self.name) # visit it
+
+        print('Exploring children of ' + self.name)
         for c in self.children:
-            print('Children of ' + self.name)
             t = c.find_orbital_transfers(name, orb_transfers + 1, visited)
             if (t is not None):
-                transfers.append(t)
+                print("%d Transfers found under %s" % (t, self.name))
+                return t
 
-        if (len(transfers) > 0):
-            return min(transfers)
-        else:
-            print('Parent of ' + self.name)
-            return self.around.find_orbital_transfers(name, orb_transfers + 1, visited)
+        return self.parent.find_orbital_transfers(name, orb_transfers + 1, visited)
 
 def parse_space_objects():
     com = None
     objects = {}
-    # for line in fileinput.input():
-    for line in lines:
+    for line in fileinput.input():
+    # for line in lines:
         [a, b] = line.strip().split(')')
         obj_a = objects.get(a)
         if obj_a is None:
@@ -75,4 +81,4 @@ com.display()
 you = com.find_descendant('YOU')
 print('&&&&&&&')
 transfers = you.find_orbital_transfers('SAN')
-print(transfers)
+print(transfers - 2)
